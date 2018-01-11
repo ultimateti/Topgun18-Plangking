95d7e22f7aa640721b4e8f6a4baaf0bb322a51c3
cybergartar <kamonnop.a@gmail.com>
2018-01-11 21:00:41 +0700

    [MISC] Clear out unnecessary files

--- node/SmartPark/models/Humidity.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var HumiditySchema = new mongoose.Schema({
-  sensID: String,
-  val: String,
-  date: Date
-});
-
-module.exports = mongoose.model('Humidity', HumiditySchema);
--- node/SmartPark/models/Pressure.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var PressureSchema = new mongoose.Schema({
-  sensID: String,
-  val: String,
-  date: Date
-});
-
-module.exports = mongoose.model('Pressure', PressureSchema);
