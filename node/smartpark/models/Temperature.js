3f873402f67fd45f82a8f7548d5bae682fa02349
cybergartar <kamonnop.a@gmail.com>
2018-01-11 11:02:17 +0700

    [FIX] Restructure workspace

--- node/smartpark/models/Temperature.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var TemperatureSchema = new mongoose.Schema({
-  sensID: String,
-  val: String,
-  date: Date
-});
-
-module.exports = mongoose.model('Temperature', TemperatureSchema);
