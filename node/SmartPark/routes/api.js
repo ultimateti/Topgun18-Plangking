1735c3605fdb1896bbd655fcc8d819d8d72bd004
cybergartar <kamonnop.a@gmail.com>
2018-01-12 08:25:09 +0700

    [DEV] Optimize performance

--- node/SmartPark/routes/api.js
+++ node/SmartPark/routes/api.js
@@ -41,106 +41,51 @@ router.get('/allTeamSensor/:start_time/:end_time', function(req, res, next) {
   })
 })
 
+router.get('/streamdata', function(req, res, next) {
+  var sensors = ['temperature', 'accelerometer','din1']
+  var requests = []
+  var result = {}
+  for (var i = 0; i < sensors.length; i++) {
+    requests.push(rp("http://10.0.0.10/api/" + sensors[i] + "/25/All"))
+    console.log("http://10.0.0.10/api/" + sensors[i] + "/25/All")
+  }
+
+  Promise.all(requests).then((results) => {
+    for(var i = 0; i < results.length; i++) {
+      var resj = JSON.parse(results[i]);
+      var isensor = sensors[i % sensors.length];
+     
+      if(resj.statusCode=='00') {
+        switch(isensor){
+          case 'temperature': tempctrl.save(resj.data, '25'); break;
+          case 'accelerometer': accelctrl.save(resj.data, '25'); break;
+          case 'din1': din1ctrl.save(resj.data, '25'); break;
+        }
+      }
+    }
+    return true
+  }).then(function(ret) {
+    var start_dt = '2018-01-11T17:00:00Z' 
+    var end_dt = new Date().toISOString()
+    console.log(new Date().toISOString())
+    accelctrl.filterRange(start_dt, end_dt, function(acc_res) {
+      result['accelerometer'] = acc_res
+      tempctrl.filterRange(start_dt, end_dt, function(tmp_res) {
+        result['temperature'] = tmp_res
+        din1ctrl.filterRange(start_dt, end_dt, function(din_res) {
+          result['din1'] = din_res
+          res.send(result)
+        })
+      })
+    })
+  }).catch(function(err) {
+    console.log(err)
+    res.send({data: 'error'})
+  });
+})
+
 router.get('/hello', function(req, res, next) {
   res.send('hello')
 })
-
-router.post('/postCoordinate', function(req, res, next) {
-    var data = {
-        TEAM_ID: req.body.TEAM_ID,
-        LAT: req.body.LAT,
-        LONG: req.body.LONG
-    }
-    console.log(data);
-    res.render('showpost', {data: data,title: 'Din1'})
-});
-
-router.get('/dummy/:teamID', function(req, res, next) {
-  var teamID = req.params.teamID;
-  var sensors = ['posts','albums'];
-  
-  var myRequests = [];
-  for(var i=0;i<sensors.length;i++) {
-    myRequests.push(rp("https://jsonplaceholder.typicode.com/" + sensors[i]));
-  }
-
-  var allTable = [];
-  Promise.all(myRequests).then((results) => {
-    for(var i=0;i<results.length;i++) {
-      var newTable = {
-        sensor: sensors[i],
-        teamID: teamID,
-        keys: ['userId','id','title'],
-        data: JSON.parse(results[i])
-     };
-     allTable.push(newTable);
-    }
-    console.log(allTable.length);
-    res.render('request', {
-        result: allTable,title: 'Din1'
-    });
-
-  }).catch(err => console.log("ERR: " +err));
-
-
-
-});
-
-router.get('/team/:teamID/:save', function(req, res, next) {
-  var teamID = req.params.teamID;
-  var isSave = req.params.save;
-  var sensors = ['temperature', 'accelerometer','din1']
-  var teamIDs = [];
-  if(teamID=='All') 
-    // teamIDs = ['11', '12', '13', '14', '15', '16', '18', '19', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '37', '38', '40', '43', '44', '46', '47', '48', '49', '50', '52', '53', '54', '60', '61'];
-    teamIDs = [25, 45]
-  else
-    teamIDs = [teamID];
-
-  var myRequests = [];
-  for(var i = 0; i < teamIDs.length; i++) {
-    for(var j = 0; j < sensors.length; j++) {
-      
-      myRequests.push(rp("http://10.0.0.10/api/" + sensors[j] + "/" + teamIDs[i] + "/All"));
-      console.log("http://10.0.0.10/api/" + sensors[j] + "/" + teamIDs[i] + "/All");
-    }
-  }
-
-  var allTable = [];
-  Promise.all(myRequests).then((results) => {
-    for(var i = 0; i < results.length; i++) {
-      var resj = JSON.parse(results[i]);
-      var newTable;
-      var isensor = sensors[i % sensors.length];
-      var iteamID = teamIDs[Math.floor(i / 3)];
-     
-      console.log('lul' + isensor)
-      if(resj.statusCode=='00') {
-        console.log("uiui" + iteamID + 'ii ' + isSave)
-        
-        if (isSave == 'true') {
-          switch(isensor){
-            case 'temperature': tempctrl.save(resj.data, iteamID); break;
-            case 'accelerometer': accelctrl.save(resj.data, iteamID); break;
-            case 'pressure': pressctrl.save(resj.data); break;
-            case 'humidity': humidctrl.save(resj.data); break;
-            case 'gyroscope': gyroctrl.save(resj.data); break;
-            case 'magnetometer': magnetctrl.save(resj.data); break;
-            case 'din1': din1ctrl.save(resj.data, iteamID); break;
-          }
-        }
-        
-      }
-    
-    }
-    console.log(allTable.length);
-    res.render('index', { title: 'GET all sensors complete!', desc:''})
-
-  }).catch(function(err) {
-    console.log(err)
-    res.render('index', { title: 'GET all error!', desc: err })
-  });
-  
-});
 
 module.exports = router;
